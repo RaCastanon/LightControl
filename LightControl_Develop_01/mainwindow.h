ac2b102dd39aa3fc2d37bd8ace69668f4e46ac08
Raul Castanon <raul.castanon@harman.com>
2018-02-18 19:18:28 -0600

    Cambio de idioma en los botones y los mensajes de error
    Cambio de idioma en los botones y los mensajes de error

--- LightControl_Develop_01/mainwindow.h
+++ LightControl_Develop_01/mainwindow.h
@@ -17,25 +17,25 @@
 #define DEFAULT_PWM_VALUE "50\n"
 #define DEFAULT_SIZE_INTENSITY 50
 #define DEFAULT_TIMER_ON 2000
-#define DEFAULT_VIDEO_NAME "Video window"
+#define DEFAULT_VIDEO_NAME "Ventana de Video"
 #define DEFAULT_WAITKEY_TIME 100
 #define DEFAULT_VIDEO_CAM_PORT 0
 #define DEFAULT_SETPOINT 120
 #define DEFAULT_SET_POINT "120"
-#define TIMER_ENABLED "Timer one is enabled"
-#define TIMER_DISABLED "Timer one is disabled"
-#define VIDEO_ENABLED "Video camera open"
-#define VIDEO_DISABLED "Video camera closed"
-#define CONNECTION_SUCESSFUL "Serial communication sucessful"
-#define SERIAL_CLOSED "Serial port closed"
-#define NAMED_WINDOW "Selected Image"
+#define TIMER_ENABLED "Temporalizador UNO habilitado"
+#define TIMER_DISABLED "Temporalizador UNO deshabilitado"
+#define VIDEO_ENABLED "Camara de video Habilitada"
+#define VIDEO_DISABLED "Camara de video Deshabilitada"
+#define CONNECTION_SUCESSFUL "Comunicacion serial establecida"
+#define SERIAL_CLOSED "Puerto de comunicacion cerrado"
+#define NAMED_WINDOW "Imagen Seleccionada"
 
-#define SERIAL_COMM_ERROR "Error 0: could not open serial port"
-#define ERROR_PORT_OPEN "Error 1: Port already open"
-#define ERROR_TIMER_OPEN "Error 2: Timer one is enabled"
-#define ERROR_TIMER_TOCLOSE "Error 3: Timer one is not active"
-#define ERROR_SET_POINT "Error 4: Default setPoint assigned"
-#define ERROR_SERIAL_CLOSE "Error 5: SerialPort is not open"
+#define SERIAL_COMM_ERROR "Error 0: No se logró abrir el puerto de comunicacion serial"
+#define ERROR_PORT_OPEN "Error 1: Puerto de comunicacion serial en uso"
+#define ERROR_TIMER_OPEN "Error 2: El temporalizador UNO esta Habilidato"
+#define ERROR_TIMER_TOCLOSE "Error 3: El temporalizador UNO no esta Habilitado"
+#define ERROR_SET_POINT "Error 4: Valor de referencia asignado por default"
+#define ERROR_SERIAL_CLOSE "Error 5: El puerto de comunicacion serial no esta Habilitado"
 
 namespace Ui {
 class MainWindow;
